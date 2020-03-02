0bb9c24f5ededd1450c79027e51d730eb21a72d2
hzqd <hzqdfs@gmail.com>
2020-03-02 16:57:15 +0800

    second commit

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
 use actix_web::{
-    get, 
-    web, 
-    App, 
-    HttpServer, 
+    get,
+    web,
+    App,
+    HttpServer,
     Responder,
     HttpRequest,
-    HttpResponse
+    HttpResponse,
 };
-use serde::{Deserialize};
+use serde::Deserialize;
 use actix_web::http::{header, Method, StatusCode};
-use actix_session::{Session};
+use actix_session::Session;
+use redis::Commands;
+
+fn fetch_an_integer() -> redis::RedisResult<isize> {
+    // connect to redis
+    let client = redis::Client::open("redis://127.0.0.1/")?;
+    let mut con = client.get_connection()?;
+    // throw away the result, just make sure it does not fail
+    let _ : () = con.set("my_key", 42)?;
+    // read back the key and return it.  Because the return value
+    // from the function is a result for integer this will automatically
+    // convert into one.
+    con.get("my_key")
+}
 
 #[derive(Deserialize)]
 struct Register {
@@ -25,13 +38,13 @@ async fn register(req: HttpRequest, form: web::Form<Register>) -> impl Responder
 #[get("/")]
 async fn index(session: Session, req: HttpRequest) -> HttpResponse {
     println!("{:?}", req.match_info());
-        HttpResponse::build(StatusCode::OK)
+    HttpResponse::build(StatusCode::OK)
         .content_type("text/html; charset=utf-8")
         .body(include_str!("../public/index.html"))
 }
 
 async fn getest(session: Session, req: HttpRequest) -> HttpResponse {
-        HttpResponse::build(StatusCode::OK)
+    HttpResponse::build(StatusCode::OK)
         .content_type("text/html; charset=utf-8")
         .body(include_str!("../public/index.html"))
 }
@@ -40,9 +53,9 @@ async fn getest(session: Session, req: HttpRequest) -> HttpResponse {
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new()
-            .service(index)
-            .route("register", web::get().to(register))
-        )
+        .service(index)
+        .route("register", web::get().to(register))
+    )
         .bind("127.0.0.1:8080")?
         .run()
         .await
