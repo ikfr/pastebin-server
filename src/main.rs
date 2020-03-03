64c23f118b8550dd0d91341ae142b48f1ef4e6a4
moeKiwiSAMA <me@kiwi.cat>
2020-03-03 21:53:35 +0800

    use templates

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use actix_files as fs;
 use actix_session::Session;
 use actix_web::http::StatusCode;
 use actix_web::{get, http, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use askama::Template;
 use rand::{distributions::Alphanumeric, Rng};
 use redis::{Commands, Connection};
 use serde::Deserialize;
@@ -24,18 +25,32 @@ lazy_static! {
         Mutex::new(builder::redis_client::build("redis://127.0.0.1/"));
 }
 
+#[derive(Template)]
+#[template(path = "input.html")]
+struct Input {}
+
+#[derive(Template)]
+#[template(path = "result.html")]
+struct PasteResult<'a> {
+    url: &'a str,
+    id: &'a str,
+    content: &'a str,
+}
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(index)
             .service(favicon)
+            .service(fs::Files::new("/static", "public/static/").show_files_listing())
+            .service(index)
             .service(paste_handler)
             .service(query_handler)
+            .service(raw_handler)
     })
-        .bind("0.0.0.0:8080")?
-        .run()
-        .await
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
 }
 
 #[get("/favicon.ico")]
@@ -45,11 +60,24 @@ async fn favicon() -> fs::NamedFile {
 
 #[get("/{id}")]
 async fn query_handler(req: HttpRequest, id: web::Path<String>) -> impl Responder {
-    println!("{:?}", id);
-    match id.len() {
-        5 => get_paste(&id),
-        _ => format!("Nah"),
+    PasteResult {
+        url: &req
+            .connection_info()
+            .let_imut(|ctx| format!("{}://{}", ctx.scheme(), ctx.host())),
+        id: &req.uri().to_string(),
+        content: &get_paste(&id),
     }
+    .let_imut(|paste_result| {
+        HttpResponse::build(StatusCode::OK)
+            .content_type("text/html; charset=utf-8")
+            .body(paste_result.render().unwrap())
+    })
+}
+
+#[get("/raw/{id}")]
+async fn raw_handler(req: HttpRequest, id: web::Path<String>) -> impl Responder {
+    println!("{:?}", id);
+    get_paste(&id)
 }
 
 #[post("/paste")]
@@ -71,7 +99,7 @@ async fn index(session: Session, req: HttpRequest) -> HttpResponse {
     println!("{:?}", req.match_info());
     HttpResponse::build(StatusCode::OK)
         .content_type("text/html; charset=utf-8")
-        .body(include_str!("../public/index.html"))
+        .body(Input {}.render().unwrap())
 }
 
 fn set_paste(key: &String, paste: &web::Form<Paste>) -> redis::RedisResult<()> {
