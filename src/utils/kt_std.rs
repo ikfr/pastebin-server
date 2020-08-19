b38deb9dffa4d4e419099759c6928444b595fcb5
hzqd <40200761+hzqd@users.noreply.github.com>
2020-08-19 23:01:44 +0800

    more elegant code

--- src/utils/kt_std.rs
+++ src/utils/kt_std.rs
@@ -1,62 +1,25 @@
 pub trait KtStd {
-    fn let_ref<R, F>(&self, block: F) -> R
-    where
-        F: FnOnce(&Self) -> R;
-
-    fn let_mut<R, F>(&mut self, block: F) -> R
-    where
-        F: FnMut(&mut Self) -> R;
-
-    fn let_owned<R, F>(self, block: F) -> R
-    where
-        Self: Sized,
-        F: FnOnce(Self) -> R;
-
-    fn also_ref<F>(&self, block: F) -> &Self
-    where
-        F: FnOnce(&Self);
-
-    fn also_mut<F>(&mut self, block: F) -> &mut Self
-    where
-        F: FnMut(&mut Self);
-}
-
-impl<T> KtStd for T {
-    fn let_ref<R, F>(&self, block: F) -> R
-    where
-        F: FnOnce(&Self) -> R,
-    {
+    fn let_ref<R>(&self, block: fn(&Self) -> R) -> R {
         block(self)
     }
 
-    fn let_mut<R, F>(&mut self, mut block: F) -> R
-    where
-        F: FnMut(&mut Self) -> R,
-    {
+    fn let_mut<R>(&mut self, block: fn(&mut Self) -> R) -> R {
         block(self)
     }
 
-    fn let_owned<R, F>(self, block: F) -> R
-    where
-        Self: Sized,
-        F: FnOnce(Self) -> R,
-    {
+    fn let_owned<R>(self, block: fn(Self) -> R) -> R where Self: Sized {
         block(self)
     }
 
-    fn also_ref<F>(&self, block: F) -> &Self
-    where
-        F: FnOnce(&Self),
-    {
+    fn also_ref(&self, block: fn(&Self)) -> &Self {
         block(self);
         self
     }
 
-    fn also_mut<F>(&mut self, mut block: F) -> &mut Self
-    where
-        F: FnMut(&mut Self),
-    {
+    fn also_mut(&mut self, block: fn(&mut Self)) -> &mut Self {
         block(self);
         self
     }
 }
+
+impl <T> KtStd for T {}
