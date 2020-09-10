feb02eb59fa37949ebe322f2d5d4f8cdd6e9feb6
hzqd <40200761+hzqd@users.noreply.github.com>
2020-09-10 10:04:12 +0800

    Change `fn pointer` to `closure`.

--- src/utils/kt_std.rs
+++ src/utils/kt_std.rs
@@ -1,22 +1,22 @@
 pub trait KtStd {
-    fn let_ref<R>(&self, block: fn(&Self) -> R) -> R {
+    fn let_ref<R>(&self, block: impl FnOnce(&Self) -> R) -> R {
         block(self)
     }
 
-    fn let_mut<R>(&mut self, block: fn(&mut Self) -> R) -> R {
+    fn let_mut<R>(&mut self, mut block: impl FnMut(&mut Self) -> R) -> R {
         block(self)
     }
 
-    fn let_owned<R>(self, block: fn(Self) -> R) -> R where Self: Sized {
+    fn let_owned<R>(self, block: impl FnOnce(Self) -> R) -> R where Self: Sized {
         block(self)
     }
 
-    fn also_ref(&self, block: fn(&Self)) -> &Self {
+    fn also_ref(&self, block: impl FnOnce(&Self)) -> &Self {
         block(self);
         self
     }
 
-    fn also_mut(&mut self, block: fn(&mut Self)) -> &mut Self {
+    fn also_mut(&mut self, mut block: impl FnMut(&mut Self)) -> &mut Self {
         block(self);
         self
     }
